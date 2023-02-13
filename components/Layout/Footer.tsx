c59e870a2ef6d428879a3a388e1b017a33ff333b
Philani Ncube <ncbphi001@gmail.com>
2023-02-13 18:44:25 +0200

    logo

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,20 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="py-10">
       <div className="max-w-7xl mx-auto flex px-8 lg:px-6">
-        <div className="bg-dark-blue px-8 flex items-center justify-center aspect-square">
-          <Link href="/" className="text-lg md:text-xl text-very-light-grey font-bold">
-            Uhuru <br />
-            Impact
+        <div className="bg-medium-grey px-8 flex items-center justify-center aspect-square">
+          <Link
+            href="/"
+            className="text-lg md:text-xl text-very-light-grey font-bold"
+          >
+            <img
+              src="/images/logo.png"
+              alt="logo"
+              className="w-20 aspect-square object-cover"
+            />
           </Link>
         </div>
         <div className="flex-1 flex items-center bg-very-light-grey">
           <nav className="flex flex-col md:flex-row space-x-0 md:space-x-4 px-4 lg:px-8">
-            <Link href="/" className="text-md md:text-lg font-bold text-medium-grey">Home</Link>
-            <Link href="/about" className="text-md md:text-lg font-bold text-medium-grey">About Us</Link>
-            <Link href="/contact" className="text-md md:text-lg font-bold text-medium-grey">Contact Us</Link>
+            <Link
+              href="/"
+              className="text-md md:text-lg font-bold text-medium-grey"
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className="text-md md:text-lg font-bold text-medium-grey"
+            >
+              About Us
+            </Link>
+            <Link
+              href="/contact"
+              className="text-md md:text-lg font-bold text-medium-grey"
+            >
+              Contact Us
+            </Link>
           </nav>
         </div>
       </div>
--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 
 
@@ -8,9 +9,9 @@ const Navbar = () => {
       <div className="py-3 px-8 sm:px-6 flex items-center justify-between max-w-7xl mx-auto">
         <div className="flex justify-start space-x-6 lg:space-x-20 items-center">
           <Link href="/" className="text-dark-blue font-extrabold text-xl lg:text-3xl">
-            Uhuru Impact
+            <img src="/images/logo.png" alt="logo" className="w-20 aspect-square object-cover"/>
           </Link>
-          <nav className="flex space-x-3 items-center">
+          <nav className="flex space-x-3 items-center translate-y-1">
             <Link href="/about" className="text-medium-grey font-extrabold text-lg">
                About Us
             </Link>
