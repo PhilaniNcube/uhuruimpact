ec9a5fe577c208c55450e9fb296f113b1162d6a9
Philani Ncube <ncbphi001@gmail.com>
2023-02-28 21:08:06 +0200

    align footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 
+const links = [
+  {
+    href: "/",
+    title: "Home"
+  },
+  {
+    href: "/about",
+    title: "About Us"
+  },
+  {
+    href: "/contact",
+    title: "Contact Us"
+  }
+]
+
 const Footer = () => {
   return (
     <footer className="py-10">
       <div className="max-w-7xl mx-auto flex px-8 lg:px-6">
-        <div className="bg-dark-blue px-1 flex items-center justify-center aspect-square">
+        <div className="bg-dark-blue px-1 flex items-center justify-center aspect-square overflow-hidden">
           <Link
             href="/"
             className="text-lg md:text-xl text-very-light-grey font-bold"
@@ -13,30 +28,21 @@ const Footer = () => {
             <img
               src="/images/uhuru.png"
               alt="logo"
-              className="w-36 object-cover"
+              className="w-40 md:w-44 object-cover lg:-translate-y-2"
             />
           </Link>
         </div>
         <div className="flex-1 flex items-center bg-very-light-grey">
           <nav className="flex flex-col md:flex-row space-x-0 md:space-x-4 px-4 lg:px-8">
-            <Link
-              href="/"
-              className="text-md md:text-lg font-bold text-medium-grey"
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              className="text-md md:text-lg font-bold text-medium-grey"
-            >
-              About Us
-            </Link>
-            <Link
-              href="/contact"
-              className="text-md md:text-lg font-bold text-medium-grey"
-            >
-              Contact Us
-            </Link>
+            {links.map((link, i) => (
+              <Link
+                key={i}
+                href={link.href}
+                className="text-xl md:text-xl lg:text-3xl font-bold text-medium-grey"
+              >
+                {link.title}
+              </Link>
+            ))}
           </nav>
         </div>
       </div>
