67408382375a8834e3acf663b93eeabe29296866
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-09 14:00:43 +0200

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import {League_Spartan} from '@next/font/google'
 import Navbar from '@/components/Layout/Navbar'
 import Footer from '@/components/Layout/Footer'
+import Script from "next/script"
 
 const spartan = League_Spartan({
   display: 'swap',
@@ -20,12 +21,25 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LJQ8WRK604"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LJQ8WRK604');
+        `}
+        </Script>
+      </head>
       <body>
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
